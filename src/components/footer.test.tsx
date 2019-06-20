c87ec678d3029fefbdac72b981cb4ef8b920d494
Ben Bracamonte <ben.bracamonte@gmail.com>
2019-06-19 21:54:25 -0500

    tests pass

---
+++ src/components/footer.test.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import Footer from './footer'
+import ReactDOM from "react-dom";
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<Footer />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
---
+++ src/components/header.test.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import Header from './header'
+import ReactDOM from "react-dom";
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<Header />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
