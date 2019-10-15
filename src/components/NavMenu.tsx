a8cc4e8058f447621b6f2e8e79e3f10a740cdcaf
brac <ben.bracamonte@gmail.com>
2019-10-15 18:40:00 -0500

    nav stuff

--- src/components/NavMenu.tsx
+++ src/components/NavMenu.tsx
@@ -3,15 +3,15 @@ import './NavStyles.sass'
 
 const NavMenu = () => {
   return (
-    <div className="  ">
-    	<ul>
+    <div className="nav">
+    	<ul className="nav-list">
         <li><a href="#welcome">Welcome</a></li>
         <li><a href="#about">About</a></li>
         <li><a href="#services">Services</a></li>
         <li><a href="#lgbtq">LGBTQ</a></li>
         <li><a href="#consultation">Consultation</a></li>
         <li><a href="#social">Social</a></li>
-        <li><a href="#contact">Contactl</a></li>
+        <li><a href="#contact">Contact</a></li>
       </ul>
     </div>
   )
