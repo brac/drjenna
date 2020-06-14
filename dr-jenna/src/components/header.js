cecb65d6db50e538b11a2c4b0ede3a0a2d672100
Ben Bracamonte <ben.bracamonte@gmail.com>
2020-06-14 16:41:49 -0500

    header and styling

--- dr-jenna/src/components/header.js
+++ dr-jenna/src/components/header.js
@@ -1,33 +1,44 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import headerStyles from "./header.module.css"
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `teal`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
+  <header>
+    <div className={headerStyles.header_container}>
+      <h1>
         <Link
+          className={headerStyles.header_main_text}
           to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
         >
           {siteTitle}
         </Link>
       </h1>
-      <div>Nav=</div>
+      
+      
+        <ul  
+          className={headerStyles.header_nav}
+        >
+          <li>
+            <p><Link to="/about/">About</Link></p>
+          </li>
+          <li>
+            <p>
+              FAQS
+            </p>
+          </li>
+          <li>
+            <p>
+              Contact
+            </p>
+          </li>
+          <li>
+            <p>
+              LGBTQ
+            </p>
+          </li>
+        </ul>
+      
     </div>
   </header>
 )
